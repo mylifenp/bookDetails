7d8932c7c640bc98ef1dfaac4a4c5ae0f42d0309
Amit Shah <mylifenp@yahoo.com>
2022-09-13 23:13:04 +0200

    Inital commit

---
+++ src/test/java/de/datenium/tutorials/bookDetails/BookDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.datenium.tutorials.bookDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
